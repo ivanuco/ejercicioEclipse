a5553c724d2096e5b8eafa7d96686c6e77c168a4
Iván del Viejo <ivandelviejo@gmail.com>
2018-02-08 18:53:44 +0100

    Hola inicial

---
+++ src/ejercicioEclipse/Hola.java
@@ -0,0 +1,15 @@
+package ejercicioEclipse;
+
+public class Hola {
+
+	public Hola() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hola");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hola hi = new Hola();
+	}
+
+}
